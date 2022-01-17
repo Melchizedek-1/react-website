16375f9c23a2b25c71125c95bfa20df92fa04b40
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2022-01-18 01:56:51 +0300

    Faqs section

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineLibraryBooks } from 'react-icons/md';
+import Question from './Question';
+import { questions } from './data';
 
 const Faq = () => {
     return (
-        <div>
-            <h1>Faqs</h1>
-        </div>
+        <section id='faq'>
+            <div className='container faq'>
+                <div className='u-title'>
+                    <MdOutlineLibraryBooks color='orangered' size={30} />
+                    <h2>FAQs</h2>
+                    <p className='u-text-small u-text-dark'>
+                        SocialX app has Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi labore, harum officiis error quia cupoditate?
+                    </p>
+                </div>
+                <div className='questions'>
+                    {
+                        questions.map((question) => (
+                            <Question title={question.title} answer={question.answer} />
+                        ))
+                    }
+                    
+                </div>
+            </div>
+        </section>
     )
 }
 
---
+++ src/components/faq/data.js
@@ -0,0 +1,22 @@
+export const questions = [
+    {
+        id: 1,
+        title: "Designed For You",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae veniam dolorum repellendus obcaecati minus voluptas neque dolore similique.",
+    },
+    {
+        id: 2,
+        title: "Keep your hitory",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae veniam dolorum repellendus obcaecati minus voluptas neque dolore similique.",
+    },
+    {
+        id: 3,
+        title: "Stay Connected",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae veniam dolorum repellendus obcaecati minus voluptas neque dolore similique.",
+    },
+    {
+        id: 4,
+        title: "Take control",
+        answer: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae veniam dolorum repellendus obcaecati minus voluptas neque dolore similique.",
+    },
+]
