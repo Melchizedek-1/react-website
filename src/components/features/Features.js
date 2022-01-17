23280685588ce7d04f4d5240bc360fbfc7c6f804
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2022-01-17 20:51:45 +0300

    Features section

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import "./Features.css";
+import { BsFillBookmarkStarFill } from 'react-icons/bs';
+import phoneFeatures from "../../assets/phone-features.png"
+import Feature from './Feature';
+import { featureList } from './data';
 
 const Features = () => {
     return (
-        <div>
-            <h1>Features</h1>
-        </div>
-    )
-}
+        <section id='features'>
+            <div className='container features'>
+                <div className='u-title'>
+                    <BsFillBookmarkStarFill color='orangered' size={30} />
+                    <h2>Core Features</h2>
+                    <p className='u-text-small u-text-dark'>
+                        SocialX app has Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi labore, harum officiis error quia cupoditate?
+                    </p>
+                </div>
+                <div className='features-content'>
+                    <div className='features-left'>
+                        <img src={phoneFeatures} alt="Phone" />
+                    </div>
+                    <div className='features-right'>
+                        {
+                            featureList.map((feature) => (
+                                <Feature key={feature.id} icon={feature.icon} heading={feature.heading} text={feature.text} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default Features
+export default Features;
