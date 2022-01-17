63233b04f3927346b421ac2c666171c689662bd4
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2022-01-18 02:31:55 +0300

    Footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import { FaMapMarkerAlt, FaPhoneAlt, FaFax, FaEnvelope, FaGlobe } from 'react-icons/fa';
+import logo from "../../assets/logo.png";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer id='footer'>
+            <div className='container footer'>
+                <div className='footer-box'>
+                    <h4>Useful Links</h4>
+                    <div className='footer-links'>
+                        <a href='#'>&bull; Support</a>
+                        <a href='#'>&bull; About</a>
+                        <a href='#'>&bull; Learn</a>
+                        <a href='#'>&bull; Hosting</a>
+                        <a href='#'>&bull; Messenger</a>
+                    </div>
+                </div>
+                <div className='footer=box'>
+                <h4>Support</h4>
+                    <div className='footer-links'>
+                        <a href='#'>&bull; Support</a>
+                        <a href='#'>&bull; About</a>
+                        <a href='#'>&bull; Learn</a>
+                        <a href='#'>&bull; Hosting</a>
+                        <a href='#'>&bull; Messenger</a>
+                    </div>
+                </div>
+                <div className='footer=box'>
+                    <h4>Contact Us</h4>
+                    <div className='footer-contact'>
+                        <p>
+                            <FaMapMarkerAlt /> &nbsp; Address: United States.
+                        </p>
+                        <p>
+                            <FaPhoneAlt /> &nbsp; Phone: +566 88989 8.
+                        </p>
+                        <p>
+                            <FaFax /> &nbsp; Fax: +7678798798.
+                        </p>
+                        <p>
+                            <FaEnvelope /> &nbsp; Email: indo@wesos.com.
+                        </p>
+                        <p>
+                            <FaGlobe /> &nbsp; Website: www.socialx.com.
+                        </p>
+                    </div>
+                </div>
+                <div className='footer=box'>
+                    <img src={logo} alt='logo' />
+                    <p className='u-small-text'>
+                        &copy; Copyright 2022. Wesonga
+                    </p>
+                </div>
+            </div>
+        </footer>
     )
 }
 
